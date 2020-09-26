c07426bffabf23cb1e92bd3f74be67bc5c458687
Daniel <daniel@it49.com>
2020-09-26 08:12:59 -0500

    jjj

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -1,215 +1,321 @@
-import React, { useRef, useEffect, useState } from 'react';
-import io from 'socket.io-client';
+import React, { useRef, useEffect, useState } from "react";
+import io from "socket.io-client";
 
 const Room = (props) => {
-	const userVideo = useRef();
-	const partnerVideo = useRef();
-	const peerRef = useRef();
-	const socketRef = useRef();
-	const otherUser = useRef([]);
-	const userStream = useRef();
-	const senders = useRef([]);
-	const partnerVideos = useRef([]);
-	const [otherVideos, setOtherVideos] = useState([]);
+  const userVideo = useRef();
+  const partnerVideo = useRef();
+  const peerRef = useRef([]);
+  const socketRef = useRef();
+  const otherUser = useRef([]);
+  const userStream = useRef();
+  const senders = useRef([]);
+  const partnerVideos = useRef([]);
+  const [otherVideos, setOtherVideos] = useState([]);
+  // const otherVideos = useRef([]);
 
-	useEffect(() => {
-		navigator.mediaDevices
-			.getUserMedia({ audio: true, video: true })
-			.then((stream) => {
-				createSteam(stream);
-			})
-			.catch((error) => {
-				navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
-					createSteam(stream);
-				});
-			});
-	}, []);
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then((stream) => {
+        createSteam(stream);
+      })
+      .catch((error) => {
+        navigator.mediaDevices
+          .getUserMedia({ audio: false, video: false })
+          .then((stream) => {
+            createSteam(stream);
+          });
+      });
+  }, []);
 
-	function createSteam(stream) {
-		userVideo.current.srcObject = stream;
-		userStream.current = stream;
+  function createSteam(stream) {
+    userVideo.current.srcObject = stream;
+    userStream.current = stream;
 
-		socketRef.current = io.connect('/');
-		socketRef.current.emit('join room', props.match.params.roomID);
+    socketRef.current = io.connect("/");
+    socketRef.current.emit("join room", props.match.params.roomID);
+    // console.log("otherUser.current---", otherUser.current);
+    socketRef.current.on("other user", (userID) => {
+      // userID.map((item) => {
+      console.log("userID", userID);
+      callUser(userID);
 
-		socketRef.current.on('other user', (userID) => {
-			console.log('other user userID', userID);
-			callUser(userID);
-			console.log('otherUser.current', otherUser.current);
-			if (otherUser.current) {
-				otherUser.current.push(userID);
-			} else {
-				otherUser.current = [userID];
-			}
-		});
-		window.roomID = props.match.params.roomID;
-		window.onbeforeunload = function (e) {
-			socketRef.current.emit('leave_room', props.match.params.roomID);
-			e.preventDefault();
-			// alert('Bye');
+      if (otherUser.current.length !== 0) {
+        console.log("yo");
+        otherUser.current.push(userID);
+      } else {
+        console.log("no--");
+        otherUser.current = [userID];
+      }
+      console.log("otherUser.current", otherUser.current);
+      // });
+    });
 
-			// return false;
-		};
+    // window.roomID = props.match.params.roomID;
+    // window.onbeforeunload = function (e) {
+    //   console.log("cerreeee");
+    //   socketRef.current.emit("leave_room", props.match.params.roomID);
+    //   e.preventDefault();
+    //   // alert('Bye');
 
-		socketRef.current.on('user joined', (userID) => {
-			console.log('otherUser.current', otherUser.current);
-			if (otherUser.current) {
-				otherUser.current.push(userID);
-			} else {
-				otherUser.current = [userID];
-			}
-		});
+    //   return false;
+    // };
+    // if (window.performance) {
+    //   console.log("hello", performance.navigation.type);
+    //   if (performance.navigation.type === 1) {
+    //     console.log("heloooooooo");
+    //   }
+    // }
 
-		socketRef.current.on('offer', handleRecieveCall);
+    socketRef.current.on("user joined", (userID) => {
+      console.log("otherUser.current --joined", otherUser.current);
+      if (otherUser.current) {
+        otherUser.current.push(userID);
+      } else {
+        otherUser.current = [userID];
+      }
+    });
 
-		socketRef.current.on('answer', handleAnswer);
+    socketRef.current.on("offer", handleRecieveCall);
 
-		socketRef.current.on('ice-candidate', handleNewICECandidateMsg);
-	}
+    socketRef.current.on("answer", handleAnswer);
 
-	function callUser(userID) {
-		peerRef.current = createPeer(userID);
-		userStream.current
-			.getTracks()
-			.forEach((track) => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
-	}
+    socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
+  }
 
-	function createPeer(userID) {
-		const peer = new RTCPeerConnection({
-			iceServers: [
-				{
-					urls: 'stun:stun.stunprotocol.org',
-				},
-				{
-					urls: 'turn:numb.viagenie.ca',
-					credential: 'muazkh',
-					username: 'webrtc@live.com',
-				},
-			],
-		});
+  function callUser(userID) {
+    console.log("callUser");
+    console.log("userID :>> ", userID);
+    peerRef.current[userID] = createPeer(userID);
+    console.log(
+      "userStream.current---------- :>> ",
+      userStream.current.getTracks()
+    );
+    userStream.current
+      .getTracks()
+      .forEach((track) =>
+        senders.current.push(
+          peerRef.current[userID].addTrack(track, userStream.current)
+        )
+      );
+  }
 
-		peer.onicecandidate = handleICECandidateEvent;
-		// peer.ontrack = handleTrackEvent(userID);
-		peer.ontrack = handleTrackEvent;
-		peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+  function createPeer(userID) {
+    console.log("createpeer :>> ", userID);
+    const peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.stunprotocol.org",
+        },
+        {
+          urls: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
+      ],
+    });
 
-		return peer;
-	}
+    peer.onicecandidate = (e) => handleICECandidateEvent(e, userID);
+    peer.ontrack = handleTrackEvent(userID);
+    // peer.ontrack = handleTrackEvent;
+    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
 
-	function handleNegotiationNeededEvent(userID) {
-		peerRef.current
-			.createOffer()
-			.then((offer) => {
-				return peerRef.current.setLocalDescription(offer);
-			})
-			.then(() => {
-				const payload = {
-					target: userID,
-					caller: socketRef.current.id,
-					sdp: peerRef.current.localDescription,
-				};
-				socketRef.current.emit('offer', payload);
-			})
-			.catch((e) => console.log(e));
-	}
+    return peer;
+  }
 
-	function handleRecieveCall(incoming) {
-		peerRef.current = createPeer();
-		const desc = new RTCSessionDescription(incoming.sdp);
-		peerRef.current
-			.setRemoteDescription(desc)
-			.then(() => {
-				userStream.current.getTracks().forEach((track) => peerRef.current.addTrack(track, userStream.current));
-			})
-			.then(() => {
-				return peerRef.current.createAnswer();
-			})
-			.then((answer) => {
-				return peerRef.current.setLocalDescription(answer);
-			})
-			.then(() => {
-				const payload = {
-					target: incoming.caller,
-					caller: socketRef.current.id,
-					sdp: peerRef.current.localDescription,
-				};
-				socketRef.current.emit('answer', payload);
-			});
-	}
+  function handleNegotiationNeededEvent(userID) {
+    let _userId = userID;
+    console.log("handleNegotiationNeededEvent userID :>> ", userID);
+    console.log("peerRef.current :>> ", peerRef.current);
+    peerRef.current[userID]
+      .createOffer()
+      .then((offer) => {
+        console.log("peerRef.current then :>> ", peerRef.current);
+        return peerRef.current[userID].setLocalDescription(offer);
+      })
+      .then(() => {
+        console.log("handleNegotiationNeededEvent then userID :>> ", userID);
+        const payload = {
+          target: _userId,
+          caller: socketRef.current.id,
+          sdp: peerRef.current[userID].localDescription,
+        };
+        socketRef.current.emit("offer", payload);
+      })
+      .catch((e) => console.log(e));
+  }
 
-	function handleAnswer(message) {
-		const desc = new RTCSessionDescription(message.sdp);
-		peerRef.current.setRemoteDescription(desc).catch((e) => console.log(e));
-	}
+  function handleRecieveCall(incoming) {
+    const userID = incoming.caller;
+    console.log("handleRecieveCall userId :>> ", userID);
+    console.log("incoming :>> ", incoming);
+    peerRef.current[userID] = createPeer(userID);
+    const desc = new RTCSessionDescription(incoming.sdp);
+    peerRef.current[userID]
+      .setRemoteDescription(desc)
+      .then(() => {
+        userStream.current
+          .getTracks()
+          .forEach((track) =>
+            peerRef.current[userID].addTrack(track, userStream.current)
+          );
+      })
+      .then(() => {
+        return peerRef.current[userID].createAnswer();
+      })
+      .then((answer) => {
+        return peerRef.current[userID].setLocalDescription(answer);
+      })
+      .then(() => {
+        const payload = {
+          target: incoming.caller,
+          caller: socketRef.current.id,
+          sdp: peerRef.current[userID].localDescription,
+        };
+        socketRef.current.emit("answer", payload);
+      });
+  }
 
-	function handleICECandidateEvent(e) {
-		if (e.candidate) {
-			otherUser.current.map((item) => {
-				const payload = {
-					target: item,
-					candidate: e.candidate,
-				};
-				socketRef.current.emit('ice-candidate', payload);
-			});
-		}
-	}
+  function handleAnswer(message) {
+    const userID = message.caller;
+    console.log("handleAnswer message :>> ", message);
+    const desc = new RTCSessionDescription(message.sdp);
+    console.log("handleAnswer peerRef.current :>> ", peerRef.current[userID]);
+    if (!peerRef.current[userID]) {
+      peerRef.current[userID] = createPeer(userID);
+    }
+    if (peerRef.current[userID])
+      peerRef.current[userID]
+        .setRemoteDescription(desc)
+        .catch((e) => console.log(e));
+    else {
+    }
+    console.log(
+      "handleAnswer peerRef.current[userID] :>> ",
+      peerRef.current[userID]
+    );
+  }
 
-	function handleNewICECandidateMsg(incoming) {
-		const candidate = new RTCIceCandidate(incoming);
+  function handleICECandidateEvent(e, userID) {
+    console.log(" handleICECandidateEvent userID :>> ", userID);
+    if (e.candidate) {
+      otherUser.current.map((item) => {
+        const payload = {
+          caller: userID,
+          target: item,
+          candidate: e.candidate,
+        };
+        socketRef.current.emit("ice-candidate", payload);
+      });
+    }
+  }
 
-		peerRef.current.addIceCandidate(candidate).catch((e) => console.log(e));
-	}
+  function handleNewICECandidateMsg(incoming) {
+    console.log(" handleNewICECandidateMsg -------arguments :>> ", arguments);
+    const userID = incoming.caller;
+    const candidate = new RTCIceCandidate(incoming.candidate);
+    console.log("handleNewICECandidateMsg userID :>> ", userID);
+    console.log("handleNewICECandidateMsg incoming :>> ", incoming);
+    console.log(
+      "handleNewICECandidateMsg peerRef.current :>> ",
+      peerRef.current
+    );
+    console.log("peerRef.current[userID] 1 :>> ", peerRef.current[userID]);
+    if (!peerRef.current[userID]) {
+      peerRef.current[userID] = createPeer(userID);
+    }
+    console.log("peerRef.current[userID] 2 :>> ", peerRef.current[userID]);
+    if (peerRef.current[userID]) {
+      peerRef.current[userID]
+        .addIceCandidate(candidate)
+        .catch((e) => console.log(e));
+    } else {
+      console.log("no existe");
+    }
+  }
 
-	function handleTrackEvent(e) {
-		partnerVideo.current.srcObject = e.streams[0];
-	}
+  //   function handleTrackEvent(e) {
+  //     partnerVideo.current.srcObject = e.streams[0];
+  //   }
 
-	// function handleTrackEvent(userID) {
-	// 	// partnerVideo.current.srcObject = e.streams[0];
-	// 	// partnerVideos.current.map((item) => (item.srcObject = e.streams[0]));
-	// 	return function (e) {
-	// 		partnerVideo.current.srcObject = e.streams[0];
-	// 		// setOtherVideos([...otherVideos, { userID, stream: e.streams[0] }]);
-	// 		// const index = partnerVideos.current.lastIndexOf();
-	// 		// partnerVideos.current[index].srcObject = e.streams[0];
-	// 	};
-	// }
+  function handleTrackEvent(userID) {
+    // console.log("userID :>> ", userID);
+    // partnerVideo.current.srcObject = e.streams[0];
+    // partnerVideos.current.map((item) => (item.srcObject = e.streams[0]));
+    return async function (e) {
+      //   partnerVideo.current.srcObject = e.streams[0];
+      // debugger;
+      // console.log("otherVideos------------------:>> ", otherVideos);
+      // console.log("userID :>> ", userID);
+      const finalOtherVideos = [...otherVideos];
+      finalOtherVideos.push({ userID, stream: e.streams[0] });
+      // console.log(">>>finalOtherVideos :>> ", finalOtherVideos);
+      partnerVideos.current.push({ userID, stream: e.streams[0] });
+      // console.log(">>>partnerVideos :>> ", partnerVideos);
+      setOtherVideos(finalOtherVideos);
+      // otherVideos.current = [
+      //   ...otherVideos.current,
+      //   { userID, stream: e.streams[0] },
+      // ];
+      //   const index = partnerVideos.current.lastIndexOf();
+      //   partnerVideos.current[index].srcObject = e.streams[0];
+    };
+  }
 
-	function shareScreen() {
-		navigator.mediaDevices.getDisplayMedia({ cursor: true }).then((stream) => {
-			const screenTrack = stream.getTracks()[0];
-			senders.current.find((sender) => sender.track.kind === 'video').replaceTrack(screenTrack);
-			screenTrack.onended = function () {
-				senders.current
-					.find((sender) => sender.track.kind === 'video')
-					.replaceTrack(userStream.current.getTracks()[1]);
-			};
-		});
-	}
+  function shareScreen() {
+    navigator.mediaDevices.getDisplayMedia({ cursor: true }).then((stream) => {
+      const screenTrack = stream.getTracks()[0];
+      senders.current
+        .find((sender) => sender.track.kind === "video")
+        .replaceTrack(screenTrack);
+      screenTrack.onended = function () {
+        senders.current
+          .find((sender) => sender.track.kind === "video")
+          .replaceTrack(userStream.current.getTracks()[1]);
+      };
+    });
+  }
 
-	function loadVideo(node, stream) {
-		console.log('loadVideo', stream);
-		node.srcObject = stream;
-	}
+  function loadVideo(node, stream) {
+    console.log("stream :>> ", stream);
+    if (node) {
+      console.log("loadVideo", stream);
+      console.log("node", node);
+      node.srcObject = stream;
+    }
+  }
 
-	console.log('return');
-	return (
-		<div>
-			<video controls style={{ height: 500, width: 500 }} autoPlay ref={userVideo} />
-			{otherVideos.map((item, index) => (
-				<video
-					key={index}
-					controls
-					style={{ height: 500, width: 500 }}
-					autoPlay
-					ref={(node) => loadVideo(node, item.stream)}
-				/>
-			))}
+  console.log("return", otherVideos);
+  return (
+    <div>
+      <h1>{socketRef.current && socketRef.current.id}</h1>
+      <video
+        controls
+        style={{ height: 500, width: 500 }}
+        autoPlay
+        // playsinline
+        ref={userVideo}
+      />
+      {partnerVideos.current.map((item, index) => (
+        <video
+          key={index}
+          controls
+          style={{ height: 500, width: 500 }}
+          autoPlay
+          // playsinline
+          ref={(node) => loadVideo(node, item.stream)}
+        />
+      ))}
 
-			<video controls style={{ height: 500, width: 500 }} autoPlay ref={partnerVideo} />
-			<button onClick={shareScreen}>Share screen</button>
-		</div>
-	);
+      {/* <video
+        controls
+        style={{ height: 500, width: 500 }}
+        autoPlay
+        ref={partnerVideo}
+      /> */}
+      <button onClick={shareScreen}>Share screen</button>
+    </div>
+  );
 };
 
 export default Room;
--- server.js
+++ server.js
@@ -1,53 +1,104 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const socket = require('socket.io');
+const socket = require("socket.io");
 const io = socket(server);
 
-const rooms = {};
+// const rooms = {};
 
-io.on('connection', (socket) => {
-	socket.on('join room', (roomID) => {
-		if (rooms[roomID]) {
-			rooms[roomID].push(socket.id);
-		} else {
-			rooms[roomID] = [socket.id];
-		}
-		console.log('socket.id', socket.id);
-		const otherUsers = rooms[roomID].filter((id) => id !== socket.id);
-		console.log('otherUsers', otherUsers);
-		if (otherUsers) {
-			otherUsers.map((item) => {
-				try {
-					console.log('item', item);
-					socket.emit('other user', item);
-					socket.to(item).emit('user joined', socket.id);
-				} catch (error) {
-					console.log('[join room error]', error);
-				}
-			});
-		}
-	});
+io.on("connection", (socket) => {
+  socket.on("join room", (roomID) => {
+    const room = roomID;
+    socket.room = room;
+    // if (rooms[roomID]) {
+    //   rooms[roomID].push(socket.id);
+    // } else {
+    //   rooms[roomID] = [socket.id];
+    // }
+    io.of("/")
+      .in(room)
+      .clients(function (error, clients) {
+        console.log("clients :>> ", clients.length);
+        // console.log("arguments :>> ", arguments);
+        console.log("socket.id", socket.id);
+        const numClients = clients.length;
+        if (numClients === 0) {
+          socket.join(room);
+        } else {
+          socket.join(room);
+        }
+        // const otherUsers = rooms[roomID].filter((id) => id !== socket.id);
+        // console.log("otherUsers", otherUsers);
+        // console.log("socket.id ----:>> ", socket.id);
+        // console.log("clients---- :>> ", clients);
 
-	socket.on('offer', (payload) => {
-		io.to(payload.target).emit('offer', payload);
-	});
+        // socket.to(socket.room).emit("other user", socket.id);
+        // socket.to(socket.room).emit("user joined", item);
 
-	socket.on('answer', (payload) => {
-		io.to(payload.target).emit('answer', payload);
-	});
+        if (clients.length !== 0) {
+          console.log("clients----if :>> ", clients);
+          clients.map((item) => {
+            try {
+              socket.emit("other user", item);
+              socket.to(socket.room).emit("user joined", socket.id);
+              console.log("item :>> ", item);
+            } catch (error) {
+              console.log("error uniendo a la room", error);
+            }
+          });
+        }
+        // if (otherUsers) {
+        //   otherUsers.map((item) => {
+        //     try {
+        //       console.log("item", item);
+        //       socket.emit("other user", item);
+        //       socket.to(item).emit("user joined", socket.id);
+        //     } catch (error) {
+        //       console.log("[join room error]", error);
+        //     }
+        //   });
+        // }
+      });
+  });
 
-	socket.on('ice-candidate', (incoming) => {
-		io.to(incoming.target).emit('ice-candidate', incoming.candidate);
-	});
+  socket.on("offer", (payload) => {
+    io.to(payload.target).emit("offer", payload);
+  });
 
-	socket.on('leave_room', function (roomID) {
-		console.log('leave_room', arguments);
-		console.log('roomID', roomID);
-		if (rooms[roomID]) rooms[roomID] = rooms[roomID].filter((item) => item !== socket.id);
-		socket.disconnect();
-	});
+  socket.on("answer", (payload) => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", (incoming) => {
+    io.to(incoming.target).emit("ice-candidate", incoming);
+  });
+
+  socket.on("leave_room", function (roomID) {
+    console.log("leave_room", arguments);
+    console.log("roomID", roomID);
+    console.log("rooms leave", rooms[roomID]);
+
+    if (rooms[roomID]) {
+      rooms[roomID] = rooms[roomID].filter((item) => item !== socket.id);
+    }
+
+    socket.disconnect();
+  });
+
+  //   socket.on("disconnect", () => {
+  //     console.log("jeannnnnnnnn");
+  //   });
+  socket.on("message", (message) => {
+    console.log("message", message);
+  });
 });
 
-server.listen(8000, () => console.log('server is running on port 8000'));
+// app.use(express.static(`${__dirname}/public`));
+
+// app.get("/*", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+
+const PORT = 8000;
+server.listen(PORT, () => console.log("server is running on port " + PORT));
