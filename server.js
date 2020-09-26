e61f08a74ddf3fe34070079e0c5d38b58ccfa1bf
jmen95 <jmen95@misena.edu.co>
2020-09-26 08:41:40 -0500

    agregar variables de entorno y confguracion para producción

--- server.js
+++ server.js
@@ -1,104 +1,109 @@
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
+require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
-const socket = require("socket.io");
+const socket = require('socket.io');
 const io = socket(server);
 
 // const rooms = {};
 
-io.on("connection", (socket) => {
-  socket.on("join room", (roomID) => {
-    const room = roomID;
-    socket.room = room;
-    // if (rooms[roomID]) {
-    //   rooms[roomID].push(socket.id);
-    // } else {
-    //   rooms[roomID] = [socket.id];
-    // }
-    io.of("/")
-      .in(room)
-      .clients(function (error, clients) {
-        console.log("clients :>> ", clients.length);
-        // console.log("arguments :>> ", arguments);
-        console.log("socket.id", socket.id);
-        const numClients = clients.length;
-        if (numClients === 0) {
-          socket.join(room);
-        } else {
-          socket.join(room);
-        }
-        // const otherUsers = rooms[roomID].filter((id) => id !== socket.id);
-        // console.log("otherUsers", otherUsers);
-        // console.log("socket.id ----:>> ", socket.id);
-        // console.log("clients---- :>> ", clients);
+io.on('connection', (socket) => {
+	socket.on('join room', (roomID) => {
+		const room = roomID;
+		socket.room = room;
+		// if (rooms[roomID]) {
+		//   rooms[roomID].push(socket.id);
+		// } else {
+		//   rooms[roomID] = [socket.id];
+		// }
+		io.of('/')
+			.in(room)
+			.clients(function (error, clients) {
+				console.log('clients :>> ', clients.length);
+				// console.log("arguments :>> ", arguments);
+				console.log('socket.id', socket.id);
+				const numClients = clients.length;
+				if (numClients === 0) {
+					socket.join(room);
+				} else {
+					socket.join(room);
+				}
+				// const otherUsers = rooms[roomID].filter((id) => id !== socket.id);
+				// console.log("otherUsers", otherUsers);
+				// console.log("socket.id ----:>> ", socket.id);
+				// console.log("clients---- :>> ", clients);
 
-        // socket.to(socket.room).emit("other user", socket.id);
-        // socket.to(socket.room).emit("user joined", item);
+				// socket.to(socket.room).emit("other user", socket.id);
+				// socket.to(socket.room).emit("user joined", item);
 
-        if (clients.length !== 0) {
-          console.log("clients----if :>> ", clients);
-          clients.map((item) => {
-            try {
-              socket.emit("other user", item);
-              socket.to(socket.room).emit("user joined", socket.id);
-              console.log("item :>> ", item);
-            } catch (error) {
-              console.log("error uniendo a la room", error);
-            }
-          });
-        }
-        // if (otherUsers) {
-        //   otherUsers.map((item) => {
-        //     try {
-        //       console.log("item", item);
-        //       socket.emit("other user", item);
-        //       socket.to(item).emit("user joined", socket.id);
-        //     } catch (error) {
-        //       console.log("[join room error]", error);
-        //     }
-        //   });
-        // }
-      });
-  });
+				if (clients.length !== 0) {
+					console.log('clients----if :>> ', clients);
+					clients.map((item) => {
+						try {
+							socket.emit('other user', item);
+							socket.to(socket.room).emit('user joined', socket.id);
+							console.log('item :>> ', item);
+						} catch (error) {
+							console.log('error uniendo a la room', error);
+						}
+					});
+				}
+				// if (otherUsers) {
+				//   otherUsers.map((item) => {
+				//     try {
+				//       console.log("item", item);
+				//       socket.emit("other user", item);
+				//       socket.to(item).emit("user joined", socket.id);
+				//     } catch (error) {
+				//       console.log("[join room error]", error);
+				//     }
+				//   });
+				// }
+			});
+	});
 
-  socket.on("offer", (payload) => {
-    io.to(payload.target).emit("offer", payload);
-  });
+	socket.on('offer', (payload) => {
+		io.to(payload.target).emit('offer', payload);
+	});
 
-  socket.on("answer", (payload) => {
-    io.to(payload.target).emit("answer", payload);
-  });
+	socket.on('answer', (payload) => {
+		io.to(payload.target).emit('answer', payload);
+	});
 
-  socket.on("ice-candidate", (incoming) => {
-    io.to(incoming.target).emit("ice-candidate", incoming);
-  });
+	socket.on('ice-candidate', (incoming) => {
+		io.to(incoming.target).emit('ice-candidate', incoming);
+	});
 
-  socket.on("leave_room", function (roomID) {
-    console.log("leave_room", arguments);
-    console.log("roomID", roomID);
-    console.log("rooms leave", rooms[roomID]);
+	socket.on('leave_room', function (roomID) {
+		console.log('leave_room', arguments);
+		console.log('roomID', roomID);
+		console.log('rooms leave', rooms[roomID]);
 
-    if (rooms[roomID]) {
-      rooms[roomID] = rooms[roomID].filter((item) => item !== socket.id);
-    }
+		if (rooms[roomID]) {
+			rooms[roomID] = rooms[roomID].filter((item) => item !== socket.id);
+		}
 
-    socket.disconnect();
-  });
+		socket.disconnect();
+	});
 
-  //   socket.on("disconnect", () => {
-  //     console.log("jeannnnnnnnn");
-  //   });
-  socket.on("message", (message) => {
-    console.log("message", message);
-  });
+	//   socket.on("disconnect", () => {
+	//     console.log("jeannnnnnnnn");
+	//   });
+	socket.on('message', (message) => {
+		console.log('message', message);
+	});
 });
 
-// app.use(express.static(`${__dirname}/public`));
+const NODE_ENV = process.env.NODE_ENV;
+const isProd = NODE_ENV === 'production';
+if (isProd) {
+	app.use(express.static(`${__dirname}/client/build`));
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
+	app.get('/*', (req, res) => {
+		res.sendFile(__dirname + '/client/build/index.html');
+	});
+}
 
-const PORT = 8000;
-server.listen(PORT, () => console.log("server is running on port " + PORT));
+const PORT = process.env.PORT;
+server.listen(PORT, () => console.log('server is running on port ' + PORT));
